e77e054cfd35154660c80b39a70560410aa43b7b
shreysoral <shreysoral@gmail.com>
2023-08-27 02:20:56 +0530

    first commit

---
+++ android/app/src/main/kotlin/com/example/TodoApp/todo_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.TodoApp.todo_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/constants/colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+const Color tdRed=Color(0xFFDA4040);
+const Color tdBlue=Color(0xFF5F52EE);
+
+const Color tdBlack=Color.fromARGB(255, 0, 0, 0);
+const Color tdGrey=Color(0xFF717171);
+
+const Color tdBGColor=Color.fromARGB(255, 255, 255, 255);
